4fdf28fcec2348a15ea97de6c17a07eddd998c41
TokJingXian <71538397+TokJingXian@users.noreply.github.com>
2020-09-26 11:37:17 +0800

    Add files via upload

--- lib/views/SearchBus.dart
+++ lib/views/SearchBus.dart
@@ -6,7 +6,19 @@ class SearchBus extends StatefulWidget {
 }
 
 class _SearchBusState extends State<SearchBus> {
-  
+  final busNoController = TextEditingController();
+  final fromTextController = TextEditingController();
+  final toTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    busNoController.dispose();
+    fromTextController.dispose();
+    toTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +32,7 @@ class _SearchBusState extends State<SearchBus> {
                 //child: Text('Hello World!'),
               ),
               TextFormField(
+                controller: busNoController,
                 decoration: const InputDecoration(
                   hintText: 'Search for Bus no.',
                   border: OutlineInputBorder(),
@@ -31,6 +44,7 @@ class _SearchBusState extends State<SearchBus> {
                 //child: Text('Hello World!'),
               ),
               TextFormField(
+                controller: fromTextController,
                 decoration: const InputDecoration(
                   labelText: 'From',
                   hintText: 'Search Bus Stop',
@@ -42,6 +56,7 @@ class _SearchBusState extends State<SearchBus> {
                 //child: Text('Hello World!'),
               ),
               TextFormField(
+                controller: toTextController,
                 decoration: const InputDecoration(
                   labelText: 'To',
                   hintText: 'Search Bus Stop',
@@ -52,8 +67,26 @@ class _SearchBusState extends State<SearchBus> {
                 padding: EdgeInsets.all(10.0),
                 //child: Text('Hello World!'),
               ),
+              Divider(
+                color: Colors.black26,
+                thickness: 1.5,
+              )
             ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            return showDialog(
+              context: context,
+              builder: (context){
+                return AlertDialog(
+                    content: Text('Values:\n' + busNoController.text + '\n' + toTextController.text + '\n' + toTextController.text),
+                );
+              },
+            );
+          },
+        tooltip: 'Show me the Values',
+        child: Icon(Icons.add),
       ),
     );
   }
--- lib/views/SearchMRT.dart
+++ lib/views/SearchMRT.dart
@@ -6,14 +6,69 @@ class SearchMRT extends StatefulWidget {
 }
 
 class _SearchMRTState extends State<SearchMRT> {
+
+  final fromTextController = TextEditingController();
+  final toTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    fromTextController.dispose();
+    toTextController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: new AppBar(
-      //   title:new Text("AppName (TBC)"),
-      // ),
-      body:new Center(
-          child: new Text("This is Search MRT Page")
+      body: Container(
+        margin: const EdgeInsets.only(right: 15, left: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              //child: Text('Hello World!'),
+            ),
+            TextFormField(
+              controller: fromTextController,
+              decoration: const InputDecoration(
+                labelText: 'From',
+                hintText: 'Search Bus Stop',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              //child: Text('Hello World!'),
+            ),
+            TextFormField(
+              controller: toTextController,
+              decoration: const InputDecoration(
+                labelText: 'To',
+                hintText: 'Search Bus Stop',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              //child: Text('Hello World!'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          return showDialog(
+            context: context,
+            builder: (context){
+              return AlertDialog(
+                content: Text('Values:\n' + toTextController.text + '\n' + toTextController.text),
+              );
+            },
+          );
+        },
+        tooltip: 'Show me the Values',
+        child: Icon(Icons.add),
       ),
     );
   }
