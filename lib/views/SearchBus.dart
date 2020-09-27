954de1fe8bd03068f6bd8be120765c8482cf8de3
TokJingXian <71538397+TokJingXian@users.noreply.github.com>
2020-09-27 12:18:15 +0800

    Add files via upload

--- lib/views/SearchBus.dart
+++ lib/views/SearchBus.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/db/database_helper.dart';
+import 'package:flutter_app/db/dbhelper.dart';
+import 'package:flutter_app/models/Route.dart';
 
 class SearchBus extends StatefulWidget {
   @override
@@ -20,9 +22,19 @@ class _SearchBusState extends State<SearchBus> {
     super.dispose();
   }
 
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  //Routes route = new Routes("", "", "", 0,0);
+  String busNo;
+  String fromStop;
+  String toStop;
+  double fare;
+  int tripID;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       body: Container(
           margin: const EdgeInsets.only(right: 15, left: 15),
           child: Column(
@@ -95,16 +107,38 @@ class _SearchBusState extends State<SearchBus> {
   }
 
   void testObject() async{
-    int i = await DatabaseHelper.instance.insert({
-      DatabaseHelper.busNo : busNoController.text,
-      DatabaseHelper.fromStop: fromTextController.text,
-      DatabaseHelper.toStop: toTextController.text,
-      DatabaseHelper.fare: 5.0, //hard-coded for now
-      DatabaseHelper.tripID: 1 //hard-coded for now
-    });
-    print('the inserted id is $i');
+    // int i = await DatabaseHelper.instance.insert({
+    //   DatabaseHelper.busNo : busNoController.text,
+    //   DatabaseHelper.fromStop: fromTextController.text,
+    //   DatabaseHelper.toStop: toTextController.text,
+    //   DatabaseHelper.fare: 5.0, //hard-coded for now
+    //   DatabaseHelper.tripID: 1 //hard-coded for now
+    // });
+    // print('the inserted id is $i');
+    //
+    // List<Map<String,dynamic>> queryRows = await DatabaseHelper.instance.queryAll();
+    // print(queryRows);
+    busNo = busNoController.text;
+    fromStop = fromTextController.text;
+    toStop = toTextController.text;
+    //fare = '5';
+    //tripID = '1';
 
-    List<Map<String,dynamic>> queryRows = await DatabaseHelper.instance.queryAll();
-    print(queryRows);
+    //var route = Routes(1,busNo,fromStop,toStop,5,1);
+    var dbHelper = DBHelper();
+    int i = await dbHelper.saveRoute({
+      DBHelper.busNo : busNoController.text,
+      DBHelper.fromStop: fromTextController.text,
+      DBHelper.toStop: toTextController.text,
+      DBHelper.fare: 5.0, //hard-coded for now
+      DBHelper.tripID: 1 //hard-coded for now
+    });
+    _showSnackBar("Data saved successfully");
+    var route = Routes(i,busNo,fromStop,toStop,5,1);
+  }
+
+  void _showSnackBar(String text) {
+    scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text(text)));
   }
 }
