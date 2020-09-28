a9f83e2973a4301c2de0501feb7bfa5e4976fa01
EugeneTan97 <eugenetan1013@gmail.com>
2020-09-28 17:23:41 +0800

    Removed the dbhelper duplicate method

--- lib/db/dbhelper.dart
+++ lib/db/dbhelper.dart
@@ -138,10 +138,4 @@ class DBHelper{
     var dbClient = await db;
     return await dbClient.delete(_tablename,where:'$routeID = ?',whereArgs:[id]);
   }
-
-  Future<List<Map>> getFaresByTripsID(int id) async{
-    var dbClient = await db;
-    List<Map> list = await dbClient.rawQuery('SELECT SUM(fare) FROM $_tablename WHERE $tripID LIKE  "%$id%"');
-    return list;
-  }
 }
