1b4828d8a711c0a138ade293142b296a04f4ece3
TokJingXian <71538397+TokJingXian@users.noreply.github.com>
2020-09-28 21:21:49 +0800

    Add files via upload

--- lib/views/CompareTrips.dart
+++ lib/views/CompareTrips.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/db/dbhelper.dart';
 
 class CompareTrips extends StatefulWidget {
   // _CompareTrips createState() => _CompareTrips();
-
 
   @override
   _CompareTripsState createState() => _CompareTripsState();
@@ -18,12 +18,24 @@ class _CompareTripsState extends State<CompareTrips> {
   List<double> _originalDBPrice = [2.10, 4.00, 5.00, 1.00]; // <-- the number of list must be followed buy the number of trips from DB
 
   List <double> selectedPrice =[2.10, 4.00]; //<-- temp storage to save the dropdown selected Price
-  List<int> selectedTrip = [1, 2]; //<-- this is temp storage for dropdownlist to separate onchange
+  List<int> selectedTrip = [1, 1]; //<-- this is temp storage for dropdownlist to separate onchange
+  double totalFares;
+
+  Future<double> fetchRoutesByTripIdFromDatabase(int id) async {
+    var dbHelper = DBHelper();
+    List<Map> list = await dbHelper.getFaresByTripsID(id);
+    totalFares = list[0]['SUM(fare)'];
+    print(totalFares);
+    return totalFares;
+  }
+
+  void updateValues(int index,double totalFare){
+    selectedPrice[index] = totalFare;
+  }
 
   Widget buildTripCard(BuildContext context, int index) {
     final trip = numCompare[index];
     return new Container(
-
         margin: EdgeInsets.all(24),
         child: Form(
           child: Column(
@@ -60,22 +72,35 @@ class _CompareTripsState extends State<CompareTrips> {
                       onChanged: (int newValue) {
                         setState(() {
                           selectedTrip[index] = newValue;
-
-                          selectedPrice[index] = _originalDBPrice[newValue -1]; // update the selected price
+                          //selectedPrice[index] = _originalDBPrice[newValue - 1]; // update the selected price
                         });
                       },
-
                       value: selectedTrip[index],
-
-
                     ),
                     SizedBox(
                       height: 20.0,
                     ),
                     Padding(padding: EdgeInsets.only(bottom: 70)),
-                    Text('SGD ${_originalDBPrice[getindex(selectedTrip[index])]}',
-                        style: TextStyle(
-                          fontSize: 20,)),
+                    // Text('SGD ${_originalDBPrice[getindex(selectedTrip[index])]}',
+                    //     style: TextStyle(
+                    //       fontSize: 20,)),
+                    //Text('Hello'),
+                    FutureBuilder(
+                      future: fetchRoutesByTripIdFromDatabase(selectedTrip[index]),
+                      builder: (context, snapshot) {
+                        if(snapshot.hasData){
+                          updateValues(index,snapshot.data);
+                          return Text(snapshot.data.toStringAsFixed(2),
+                          style: TextStyle(fontSize: 20));
+                        }else if (snapshot.hasError) {
+                          return Text("${snapshot.error}");
+                        } else if(snapshot.data == null){
+                          updateValues(index,0);
+                          return Text("No route in trip");
+                        }
+                          return CircularProgressIndicator();
+                        }
+                    ),
                   ],
                 ),
                 Divider(
@@ -90,16 +115,13 @@ class _CompareTripsState extends State<CompareTrips> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Builder(
-
         builder: (_) {
           return new Container(
-
-            margin: EdgeInsets.all(24),
+            //margin: EdgeInsets.all(24),
             child: Form(
               child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  //mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     SizedBox(
                       height: 300,
@@ -113,18 +135,14 @@ class _CompareTripsState extends State<CompareTrips> {
                     ),
                   ]),
             ),
-
-
           );
         },
       ),
-
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             return showDialog(
                 context: context,
                 builder: (context) {
-
                   return AlertDialog(
                     title: Text("Message"),
                     content: Column(
@@ -155,12 +173,17 @@ class _CompareTripsState extends State<CompareTrips> {
             break;
           }
       }
-
     return index;
   }
 
   // compare Price
   String comparePrice(double fprice, double sprice) {
+    if(fprice == 0){
+      return "First trip is empty";
+    }
+    if(sprice == 0){
+      return "Second trip is empty";
+    }
     if (fprice < sprice) {
       double diffprice = sprice - fprice;
       return "First Trip is cheaper, you saved \$${diffprice.toStringAsFixed(2)}";
@@ -173,8 +196,6 @@ class _CompareTripsState extends State<CompareTrips> {
       return "Both trip is the same price";
     }
   }
-
-
 }
 
 
